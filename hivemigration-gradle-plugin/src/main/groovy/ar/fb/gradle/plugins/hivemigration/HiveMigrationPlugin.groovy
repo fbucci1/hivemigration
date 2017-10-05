5316ad3513d16b6fe1225748cc6ea744e9ef9b72
Fernando <fbucci1@github.com>
2017-10-05 05:29:18 +0200

    import project

---
+++ hivemigration-gradle-plugin/src/main/groovy/ar/fb/gradle/plugins/hivemigration/HiveMigrationPlugin.groovy
@@ -0,0 +1,17 @@
+package ar.fb.gradle.plugins.hivemigration
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import ar.fb.gradle.plugins.hivemigration.tasks.MigrateTask
+
+class HiveMigrationPlugin implements Plugin<Project> {
+
+	public static final NAME = HiveMigrationExtension.NAME + "-gradle-plugin"
+
+	@Override
+	void apply(Project target) {
+		HiveMigrationExtension extension = target.extensions.create(HiveMigrationExtension.NAME, HiveMigrationExtension)
+		target.task(MigrateTask.TASK_NAME, type: MigrateTask)
+	}
+}
--- shared/README.md
+++
@@ -1,4 +0,0 @@
-## shared
-
-Shared componentes
-
