c54221da4e585bf7095cf5ced59a8c84820e1373
Fernando <fbucci1@github.com>
2017-10-06 20:03:27 +0200

    remove unused imports

--- hivemigration-gradle-plugin/src/main/groovy/ar/fb/gradle/plugins/hivemigration/core/internal/utils/scripts/JDBCUtil.java
+++ hivemigration-gradle-plugin/src/main/groovy/ar/fb/gradle/plugins/hivemigration/core/internal/utils/scripts/JDBCUtil.java
@@ -21,7 +21,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
